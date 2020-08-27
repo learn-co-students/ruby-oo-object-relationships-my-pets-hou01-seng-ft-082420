a080f5fc5c5711060ddc9e7b2012911085f71250
denimcouch <alexmata92@ymail.com>
2020-08-27 14:49:41 -0500

    created Owner/Dog/Cat models with initialize methods

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name, species= "human")
+    @name = name
+    @species = species
+    self.class.all << self 
+  end
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all 
+  end
+
+  def self.count
+    self.all.size
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
 end
